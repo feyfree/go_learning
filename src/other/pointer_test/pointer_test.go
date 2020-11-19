d9560f5d084267ebfb3a356d419e202edf6b55ac
leilei <feyfree@163.com>
2020-11-19 11:35:57 +0800

    [ADD] add pointer test

---
+++ src/other/pointer_test/pointer_test.go
@@ -0,0 +1,27 @@
+package pointer_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPointer(t *testing.T) {
+	var a = 1
+	var b = &a
+	var c = &b
+	var x = *b
+	fmt.Println("a = ", a)
+	fmt.Println("&a = ", &a)
+	fmt.Println("*&a = ", *&a)
+	fmt.Println("b = ", b)
+	fmt.Println("&b = ", &b)
+	fmt.Println("*&b = ", *&b)
+	fmt.Println("*b = ", *b)
+	fmt.Println("c = ", c)
+	fmt.Println("*c = ", *c)
+	fmt.Println("&c = ", &c)
+	fmt.Println("*&c = ", *&c)
+	fmt.Println("**c = ", **c)
+	fmt.Println("***&*&*&*&c = ", ***&*&*&*&*&c)
+	fmt.Println("x = ", x)
+}
