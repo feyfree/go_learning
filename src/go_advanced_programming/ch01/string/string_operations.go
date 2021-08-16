b795a53ec02c3088f9ed249b08fd175cef0a13bb
feyfree <feyfree@163.com>
2021-08-16 21:28:29 +0800

    [ADD] 新增go语言高级编程相关学习

--- src/go_advanced_programming/ch01/string/string_operations.go
+++ src/go_advanced_programming/ch01/string/string_operations.go
@@ -6,4 +6,11 @@ func main() {
 	for i, c := range []byte("世界abc") {
 		fmt.Println(i, c)
 	}
+
+	fmt.Printf("%#v\n", []rune("世界")) // []int32{19990, 30028}
+	world := string([]rune{'世', '界'})
+	for i, c := range world {
+		fmt.Println(i, c)
+	}
+	fmt.Printf("%#v\n", world) // 世界
 }
