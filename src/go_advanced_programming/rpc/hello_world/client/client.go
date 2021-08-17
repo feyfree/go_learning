26565c24e81ace3acb00fadd7a266dfc1ba526cc
feyfree <feyfree@163.com>
2021-08-17 17:58:23 +0800

    [ADD] rpc with json

--- src/go_advanced_programming/rpc/hello_world/client/client.go
+++ src/go_advanced_programming/rpc/hello_world/client/client.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
 func main() {
-	client, err := rpc.Dial("tcp", "localhost:1234")
+	conn, err := net.Dial("tcp", "localhost:1234")
 	if err != nil {
-		log.Fatal("dialing:", err)
+		log.Fatal("net.Dial:", err)
 	}
+
+	client := rpc.NewClientWithCodec(jsonrpc.NewClientCodec(conn))
 
 	var reply string
 	err = client.Call("HelloService.Hello", "hello", &reply)
--- src/go_advanced_programming/rpc/hello_world/service/hello_world.go
+++ src/go_advanced_programming/rpc/hello_world/service/hello_world.go
@@ -4,23 +4,42 @@ import (
 	"log"
 	"net"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 )
 
+//func main() {
+//	err := rpc.RegisterName("HelloService", new(HelloService))
+//	if err != nil {
+//		return
+//	}
+//
+//	listener, err := net.Listen("tcp", ":1234")
+//	if err != nil {
+//		log.Fatal("ListenTCP error:", err)
+//	}
+//
+//	conn, err := listener.Accept()
+//	if err != nil {
+//		log.Fatal("Accept error:", err)
+//	}
+//
+//	rpc.ServeConn(conn)
+//}
+
 func main() {
-	err := rpc.RegisterName("HelloService", new(HelloService))
-	if err != nil {
-		return
-	}
+	rpc.RegisterName("HelloService", new(HelloService))
 
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Fatal("ListenTCP error:", err)
 	}
 
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal("Accept error:", err)
-	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal("Accept error:", err)
+		}
 
-	rpc.ServeConn(conn)
+		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
 }
